26f0fa9557f97589bbce14a3ee76ebc278ab307b
Ahmed Bilal Siddiqui <asiddiqui.bese-26mcs@student.nust.edu.pk>
2022-07-20 17:18:54 +0500

    margin altered

--- src/components/boxWrappers.jsx
+++ src/components/boxWrappers.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Space from "./spaceCom"
 const boxWrapper=(props)=>{
     return(
-        <div className="w-5/6 m-auto flex flex-col justify-center">
+        <div className="w-4/6 m-auto flex flex-col justify-center">
             <Space/>
             {props.children}
             <Space/>
--- src/components/imageSlider.jsx
+++ src/components/imageSlider.jsx
@@ -5,12 +5,12 @@ const imageSlider = (props) => {
     "bg-gray-500/75 flex justify-between w-12 h-12 px-4 text-xl py-2 hover:bg-black text-center hover:text-white rounded-full";
   return (
     <Wrapper>
-    <div className="h-screen bg-pink-400">
-      <div className="absolute bottom-1/4 w-5/6 flex justify-between px-3 z-10">
+    <div className="h-screen flex flex-col m-auto">
+      <div className="flex justify-between px-3 z-10">
         <button className={styleBtn}>&lt;</button>
         <button className={styleBtn}>&gt;</button>
       </div>
-      {/* <img src="//images.ctfassets.net/yadj1kx9rmg0/wtrHxeu3zEoEce2MokCSi/cf6f68efdcf625fdc060607df0f3baef/quwowooybuqbl6ntboz3.jpg" alt="" /> */}
+      <img src="//images.samsung.com/is/image/samsung/assets/pk/1440x640-without-text.png?imwidth=1366" alt="" />
     </div>
       </Wrapper>
   );
