ecde6c645b05e767b56a75d29f93a238bfb5c3f7
Ahmed Bilal Siddiqui <ahmedbilal6880@gmail.com>
2022-07-22 16:40:00 +0500

    added hover feature to offers

--- src/components/offersComps.jsx
+++ src/components/offersComps.jsx
@@ -1,15 +1,44 @@
-import { Component } from "react";
+// import { Component } from "react";
 
-class offersComps extends Component {
-  render() {
-    return (
-      <div className="bg-gray-200 flex flex-wrap flex-col items-center p-12 rounded-md hover:cursor-pointer">
-        <img src={this.props.offers[this.props.comNo].src} alt="" />
-        <p>{this.props.offers[this.props.comNo].name}</p>
-        <p>{this.props.offers[this.props.comNo].price}</p>
-      </div>
+import { useState } from "react";
+
+let OffersComps = (props) => {
+  const [scale, setScale] = useState("scale-100");
+  const [content, setContent] = useState(
+    <>
+      <p>{props.offers[props.comNo].price}</p>
+    </>
+  );
+  function hoverOffer() {
+    setContent(
+      <>
+        <button className="w-28 bg-black hover:bg-gray-700 text-white p-2 py-2.5 rounded-3xl">
+          Buy Now
+        </button>
+      </>
     );
+    setScale("scale-105");
   }
-}
+  function Offer() {
+    setContent(
+      <>
+        <p>{props.offers[props.comNo].price}</p>
+      </>
+    );
+    setScale("scale-100");
+  }
 
-export default offersComps;
+  return (
+    <div
+      className="bg-[#F4F4F4] h-[380px] flex flex-wrap flex-col items-center p-12 rounded-md hover:cursor-pointer gap-5"
+      onMouseEnter={hoverOffer}
+      onMouseLeave={Offer}
+    >
+      <img className={scale} src={props.offers[props.comNo].src} alt="" />
+      <p className="font-semibold text-xl">{props.offers[props.comNo].name}</p>
+      {content}
+    </div>
+  );
+};
+
+export default OffersComps;
--- src/components/offersMainComp.jsx
+++ src/components/offersMainComp.jsx
@@ -1,18 +1,41 @@
-import { Component } from "react";
+import { useState } from "react";
 
-class offersMainComp extends Component {
-  render() {
-    return (
-      <div className="bg-slate-200 flex flex-wrap flex-col items-center p-12 rounded-md hover:cursor-pointer">
-        <img
-          src={this.props.offers[0].src}
-          alt=""
-        />
-        <p>{this.props.offers[0].name}</p>
-        <p>{this.props.offers[0].price}</p>
-      </div>
+let OffersMainComp = (props) => {
+  const [scale, setScale] = useState("scale-100");
+  const [content, setContent] = useState(
+    <>
+      <p>{props.offers[0].price}</p>
+    </>
+  );
+  function hoverOffer() {
+    setContent(
+      <>
+        <button className="w-28 bg-black hover:bg-gray-700 text-white p-2 py-2.5 rounded-3xl">
+          Buy Now
+        </button>
+      </>
+    );
+    setScale("scale-105");
+  }
+  function Offer() {
+    setScale("scale-100");
+    setContent(
+      <>
+        <p>{props.offers[0].price}</p>
+      </>
     );
   }
-}
+  return (
+    <div
+      onMouseEnter={hoverOffer}
+      onMouseLeave={Offer}
+      className="bg-[#F4F4F4] gap-5 flex flex-wrap flex-col items-center p-12 rounded-md hover:cursor-pointer"
+    >
+      <img className={scale} src={props.offers[0].src} alt="" />
+      <p className="font-semibold text-xl">{props.offers[0].name}</p>
+      {content}
+    </div>
+  );
+};
 
-export default offersMainComp;
+export default OffersMainComp;
