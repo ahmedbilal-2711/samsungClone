275c1bb1c97684d797021d354761b3c3f5ed1336
Ahmed Bilal Siddiqui <ahmedbilal6880@gmail.com>
2022-07-23 12:38:45 +0500

    imgae slider btn set

--- src/components/imageSlider.jsx
+++ src/components/imageSlider.jsx
@@ -2,13 +2,21 @@
 import Wrapper from "./boxWrappers";
 const imageSlider = (props) => {
   const styleBtn =
-    "bg-gray-500/75 flex justify-between w-12 h-12 px-4 text-xl py-2 hover:bg-black text-center hover:text-white rounded-full";
+    "bg-gray-500/75 float-left  z-20 w-12 h-12 px-4 text-xl py-2 hover:bg-black text-center hover:text-white rounded-full";
   return (
     <Wrapper>
-      <div className="flex justify-between px-3 z-10">
-        <button className={styleBtn}>&lt;</button>
-        <button className={styleBtn}>&gt;</button>
-      </div>
+      {/* <div className="flex absolute justify-end align-bottom z-20"> */}
+      <button
+        className={
+          "absolute object-left-top right-[88%] bg-gray-500/75 content-center z-20 w-12 h-12 px-4 text-xl py-2 hover:bg-black text-center hover:text-white rounded-full"
+        }
+      >
+        &lt;
+      </button>
+      <button className="absolute object-right-top left-[88%]  bg-gray-500/75 content-center z-20 w-12 h-12 px-4 text-xl py-2 hover:bg-black text-center hover:text-white rounded-full">
+        &gt;
+      </button>
+      {/* </div> */}
       <img
         src="//images.samsung.com/is/image/samsung/assets/pk/1440x640-without-text.png?imwidth=1366"
         alt=""
